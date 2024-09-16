32c8013362b80f310f006f16f071fdc7b47e9eb2
Amro Sati <amrosatti98@gmail.com>
2024-09-16 17:06:19 +0300

    filestorage upgrade

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,9 +16,17 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if not cls:
+            return FileStorage.__objects
+
+        filtered = {}
+        for key, obj in FileStorage.__objects:
+            if type(obj) is cls:
+                filtered[key] = obj
+
+        return filtered
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -94,3 +102,13 @@ class FileStorage:
                     }
 
         return attributes
+
+    def delete(self, obj=None):
+        """Deletes `obj` from storage if present
+        """
+        if not obj:
+            return
+
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        if key in list(FileStorage.__objects.keys()):
+            del FileStorage.__objects[key]
