892763c6f6f47c3a7440cbf8de6079e1f36d6c07
Amro Sati <amrosatti98@gmail.com>
2024-09-28 21:49:49 +0300

    place

--- models/place.py
+++ models/place.py
@@ -12,17 +12,19 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
 
-    city_id = Column(String(60), nullable=False, ForeignKey("cities.id"))
-    user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
+    city_id = Column(String(60), ForeignKey('cities.id'),
+                     nullable=False) if is_db else ""
+    user_id = Column(String(60), ForeignKey('users.id'),
+                     nullable=False) if is_db else ""
     name = Column(String(128), nullable=False) if is_db else ""
     description = Column(String(1024)) if is_db else ""
     number_rooms = Column(Integer, nullable=False,
-                          default=0) is is_db else 0
+                          default=0) if is_db else 0
     number_bathrooms = Column(Integer, nullable=False,
-                              default=0) is is_db else 0
-    max_guest = Column(Integer, nullable=False, default=0) is is_db else 0
+                              default=0) if is_db else 0
+    max_guest = Column(Integer, nullable=False, default=0) if is_db else 0
     price_by_night = Column(Integer, nullable=False,
-                            default=0) is is_db else 0
+                            default=0) if is_db else 0
     latitude = Column(Float) if is_db else 0.0
     longitude = Column(Float) if is_db else 0.0
     amenity_ids = []
