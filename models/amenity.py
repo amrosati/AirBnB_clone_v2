c35a6ff00c43dd4b8779028c237735c1f9660738
Amro Sati <amrosatti98@gmail.com>
2024-09-28 22:49:38 +0300

    place amenities

--- models/amenity.py
+++ models/amenity.py
@@ -2,13 +2,18 @@
 """ State Module for HBNB project """
 import os
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 from models.base_model import BaseModel, Base
+from models.place import place_amenity
 
 is_db = os.getenv('HBNB_TYPE_STORAGE') == 'db'
 
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
+
     name = Column(String(128),
                   nullable=False) if is_db else ""
+
+    place_amenities = relationship('Place', secondary=place_amenity)
--- models/place.py
+++ models/place.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 import os
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Table, Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
 from models.base_model import BaseModel, Base
 
 is_db = os.getenv('HBNB_TYPE_STORAGE') == 'db'
 
+# Define association table of the many-to-many relationship
+place_amenity = Table(
+    'place_amenity', Base.metadata,
+    Column('place_id', String(60), ForeignKey('places.id'),
+           primary_key=True, nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'),
+           primary_key=True, nullable=False)
+)
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
 
-    city_id = Column(String(60), ForeignKey('cities.id'),
-                     nullable=False) if is_db else ""
-    user_id = Column(String(60), ForeignKey('users.id'),
-                     nullable=False) if is_db else ""
     name = Column(String(128), nullable=False) if is_db else ""
     description = Column(String(1024)) if is_db else ""
     number_rooms = Column(Integer, nullable=False,
@@ -28,12 +33,20 @@ class Place(BaseModel, Base):
                             default=0) if is_db else 0
     latitude = Column(Float) if is_db else 0.0
     longitude = Column(Float) if is_db else 0.0
-    amenity_ids = []
+
+    city_id = Column(String(60), ForeignKey('cities.id'),
+                     nullable=False) if is_db else ""
+    user_id = Column(String(60), ForeignKey('users.id'),
+                     nullable=False) if is_db else ""
 
     if is_db:
         reviews = relationship('Review', cascade='delete', backref='place')
+        amenities = relationship(
+            'Amenity', secondary=place_amenity,
+            back_populates="place_amenities", viewonly=False
+        )
     else:
-        @propery
+        @property
         def reviews(self):
             """Getter for the reviews linked with the place"""
             from models import storage
@@ -41,3 +54,21 @@ class Place(BaseModel, Base):
 
             return [obj for obj in storage.all(Review).values()
                     if obj.place_id == self.id]
+
+        @property
+        def amenities(self):
+            """Getter for the amenities linked with the place"""
+            from models import storage
+            from models.amenity import Amenity
+
+            return [obj for obj in storage.all(Amenity).values()
+                    if obj.id in amenity_ids]
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Setter for the amenities linked with the place"""
+            from models.amenity import Amenity
+
+            if type(obj) is Amenity:
+                if obj.place_id == self.id:
+                    self.amenity_ids.append(obj.id)
